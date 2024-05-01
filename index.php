bfb2f0e5709352df704ed294e13a3d457876cf06
Bruno <brunoenrrike@hotmail.com>
2024-05-01 19:38:59 -0300

    Sistema de Papeleta Eletronica.

--- index.php
+++ index.php
@@ -1,9 +1,7 @@
 <?php
   echo "Hello world..!!! <br>";
-
   echo "Olá Git Hub... <br>";
-
   echo "Aqui vai sincronizar automatico.<br>";
-
   echo "Beleza, vou salvar no visual studio code, dar um commit e atualizar a página.";
+
 ?>
