85defddbde802f30190e498593034aef2a6300a7
Bruno <brunoenrrike@hotmail.com>
2024-05-01 19:51:19 -0300

    Criação Inicial

--- index.php
+++ index.php
@@ -1,3 +1,24 @@
-<?php
-  echo "Hello world..!!!   Olá Git Hub...  Aqui vai sincronizar automatico.<br>  Beleza, vou salvar no visual studio code, dar um commit e atualizar a página.";
-?>
+<?php $diretorio = "../../"; ?>
+<!doctype html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>Noromix - Papeleta Eletrônica</title>
+		<link rel="shortcut icon" href="<?php echo $diretorio; ?>favicon.ico" />
+		<meta name='viewport' content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0' />
+
+		<meta http-equiv="Content-Language" content="pt-br">
+		<link href="<?php echo $diretorio; ?>Font-Awesome-6.x/css/all.css" rel="stylesheet">
+		<!-- DATA TABLES -->
+		<style type="text/css">
+			@import "<?php echo $diretorio; ?>DataTables/DataTables-1.10.18/css/bootstrap2.css";
+		</style>
+		<link href="<?php echo $diretorio; ?>admin/Fornecedores/links_includes/estilos_update_fornecedores.css" rel="stylesheet" type="text/css">
+	</head>
+
+	<body>
+
+
+
+  </body>
+</html>
