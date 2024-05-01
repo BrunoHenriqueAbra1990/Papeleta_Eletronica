143dcaaa78c6e33d9d8c516c089fed784504d248
Bruno <brunoenrrike@hotmail.com>
2024-05-01 19:55:53 -0300

    estilos

--- index.php
+++ index.php
@@ -13,12 +13,24 @@
 		<style type="text/css">
 			@import "<?php echo $diretorio; ?>DataTables/DataTables-1.10.18/css/bootstrap2.css";
 		</style>
-		<link href="<?php echo $diretorio; ?>admin/Fornecedores/links_includes/estilos_update_fornecedores.css" rel="stylesheet" type="text/css">
+		<link href="links_includes/estilos.css" rel="stylesheet" type="text/css">
 	</head>
 
 	<body>
+    <h1>Tela de atualização de cadastro de Fornecedores do Grupo Scamatti</h1>
+      <div class="col-md-12 form-row" style="font-size:14px;">
+        &nbsp;&nbsp;&nbsp;&nbsp;
+        <b style='color:red'>Atenção: &nbsp;</b> 
+          É imprescindível que os faturamentos, tanto de Notas de Serviço quanto de Notas de Produtos, sejam enviados para o email: &nbsp;
+        <b style='color:red'> notafiscal@escritoriovotuporanga.com.br</b>
+        
+        &nbsp;	Por favor, copie outro membro do Grupo Scamatti, se desejar, mas é fundamental que essas informações sejam centralizadas nesse email.<br>
+        &nbsp;	O não envio para este endereço pode resultar no não lançamento e no não pagamento da sua nota.
+        &nbsp;	Contamos com a sua colaboração para garantir o correto processamento dos documentos. Obrigado.
+      </div>
+      
+      <div id="conteudo"> 
 
-
-
+      </div>
   </body>
 </html>
