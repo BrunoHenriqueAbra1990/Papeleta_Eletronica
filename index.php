41169900a4b0f12ddfb84a90e9eae0a492bd7d03
Bruno <brunoenrrike@hotmail.com>
2024-05-01 19:41:38 -0300

    a

--- index.php
+++ index.php
@@ -1,4 +1,3 @@
 <?php
   echo "Hello world..!!! <br>  Olá Git Hub... <br>  Aqui vai sincronizar automatico.<br>  Beleza, vou salvar no visual studio code, dar um commit e atualizar a página.";
-
 ?>
